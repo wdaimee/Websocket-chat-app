4200b112a76cf1f80ccc5567672a4b94539b9380
Waqas D <wdaimee@gmail.com>
2020-04-27 21:57:53 -0400

    added simple functionality for adding messages when pressing submit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,2 +1,21 @@
+const sendMessageBtn = document.querySelector('.send-btn');
+const textArea = document.querySelector('.text-area');
+const chatBox = document.querySelector('.chat-box');
+const clearBtn = document.querySelector('.clear-btn');
 const socket = io();
+
 console.log(socket);
+
+sendMessageBtn.addEventListener('click', () => addMessage())
+clearBtn.addEventListener('click', function() {
+    chatBox.innerHTML = "";
+});
+
+function addMessage() {
+    let newMessage = textArea.value;
+    let newPara = document.createElement('p');
+    let text = document.createTextNode(`${newMessage}`)
+    newPara.appendChild(text);
+    chatBox.appendChild(newPara);
+    textArea.value = '';
+}
