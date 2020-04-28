4c049c3056c6ab7e98eaeef80c607fc1177e8539
Waqas D <wdaimee@gmail.com>
2020-04-28 09:58:43 -0400

    added comments to code for better readability

--- io.js
+++ io.js
@@ -1,7 +1,10 @@
 const io = require('socket.io')();
 
 io.on('connection', function(socket) {
+    //confirm connection of socket.io on backend
     console.log('Client is connected')
+
+    //when client send's server add-message message, emit add-message message to all clients
     socket.on('add-message', function(data) {
         io.emit('add-message', data);
     });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,33 +4,40 @@ const chatBox = document.querySelector('.chat-box');
 const clearBtn = document.querySelector('.clear-btn');
 const socket = io();
 
+//confirm connection of frontend to socket
 console.log(socket);
 
+//when add-message message recived from server, trigger addMessage function
 socket.on('add-message', function(data) {
     addMessage(data);
 });
 
+//event handle for when send message button pressed, send add-message data to server
 sendMessageBtn.addEventListener('click', function(evt) {
     socket.emit('add-message', {
         newMessage: textArea.value
     });
 });
 
+//event handler for clearing the screen
 clearBtn.addEventListener('click', function() {
     chatBox.innerHTML = "";
 });
 
+//prompt user for username
 do {
     username = getUsername();
 }   while (username.length < 2);
   
 // socket.emit('register-player', initials);
-  
+ 
+//get user's username
 function getUsername() {
     var input = prompt("Please enter your username");
     return input ? input : '';
 }
 
+//function for adding a message, create a <p> tag and push to chatBox section
 function addMessage({newMessage}) {
     let newPara = document.createElement('p');
     let text = document.createTextNode(`${newMessage}`)
