1df783e573e0d95c34a5f2c5ff4e2ec9a5c03e06
Waqas D <wdaimee@gmail.com>
2020-05-25 13:31:31 -0400

    added logged in user display and functionality to show which users add which comments

--- io.js
+++ io.js
@@ -1,12 +1,30 @@
 const io = require('socket.io')();
 
+//Object to hold usernames of logged in users
+const users = {};
+
 io.on('connection', function(socket) {
     //confirm connection of socket.io on backend
     console.log('Client is connected')
 
     //when client send's server add-message message, emit add-message message to all clients
     socket.on('add-message', function(data) {
-        io.emit('add-message', data);
+        io.emit('add-message', {
+            newMessage: data.newMessage,
+            user: users[data.user]
+        });
+    });
+
+    //Add new users to users object and emit to client side
+    socket.on('register-user', function(username) {
+        users[socket.id] = username;
+        io.emit('update-user-list', Object.values(users));
+    });
+
+    //Listener for when user disconnects from chat
+    socket.on('disconnect', function() {
+        delete users[socket.id];
+        io.emit('update-user-list', Object.values(users));
     });
 });
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,21 +2,30 @@ const sendMessageBtn = document.querySelector('.send-btn');
 const textArea = document.querySelector('.text-area');
 const chatBox = document.querySelector('.chat-box');
 const clearBtn = document.querySelector('.clear-btn');
+const usersArea = document.querySelector('.users');
 const socket = io();
 let username;
 
 //confirm connection of frontend to socket
 console.log(socket);
 
-//when add-message message recived from server, trigger addMessage function
+//when add-message message received from server, trigger addMessage function
 socket.on('add-message', function(data) {
     addMessage(data);
 });
 
+//Listen for when the users list has changed
+socket.on('update-user-list', function(data) {
+    console.log(data)
+    let userList = '<li>' + data.join('</li><li>') + '</li>';
+    usersArea.innerHTML = userList;
+})
+
 //event handle for when send message button pressed, send add-message data to server
 sendMessageBtn.addEventListener('click', function(evt) {
     socket.emit('add-message', {
-        newMessage: textArea.value
+        newMessage: textArea.value,
+        user: socket.id
     });
 });
 
@@ -29,8 +38,7 @@ clearBtn.addEventListener('click', function() {
 do {
     username = getUsername();
 }   while (username.length < 2);
-  
-// socket.emit('register-player', initials);
+socket.emit('register-user', username);
  
 //get user's username
 function getUsername() {
@@ -39,9 +47,9 @@ function getUsername() {
 }
 
 //function for adding a message, create a <p> tag and push to chatBox section
-function addMessage({newMessage}) {
+function addMessage({newMessage, user}) {
     let newPara = document.createElement('p');
-    let text = document.createTextNode(`${username}: ${newMessage}`)
+    let text = document.createTextNode(`${user}: ${newMessage}`)
     newPara.appendChild(text);
     chatBox.appendChild(newPara);
     textArea.value = '';
